4cfddcdb288079edb3d4e47510f1115f0aa649d2
dariomontani <dariomontani@gmail.com>
2022-01-19 11:52:32 +0100

    init

---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+    name: 'Main',
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
