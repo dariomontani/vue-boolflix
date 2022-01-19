a7e700602fc5c2e487fafa9e1c6f8b3ccff5293e
dariomontani <dariomontani@gmail.com>
2022-01-19 12:02:48 +0100

    add searchbar

--- src/App.vue
+++ src/App.vue
@@ -20,5 +20,5 @@ export default {
 </script>
 
 <style lang="scss">
-  
+  @import '~bootstrap/scss/bootstrap';
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,34 @@
 <template>
-    <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
+<div class="row">
+    <div class="col-6">
+        <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+    </div>
+</div>
+
+<!-- Milestone 1:
+Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il  bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
+Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato: 
+Titolo
+Titolo Originale
+Lingua
+Voto -->
+
 </template>
 
 <script>
 export default {
     name: 'Header',
     data(){
-        return
+        return{
+
+        }
     }
 }
 </script>
 
 <style lang='scss'>
-    @import '~bootstrap/scss/bootstrap';
+    
 </style>
