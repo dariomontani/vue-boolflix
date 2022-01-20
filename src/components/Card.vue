ed3b797e616b0403dc556e8f9b6e73486e9c518c
dariomontani <dariomontani@gmail.com>
2022-01-20 16:39:21 +0100

    aggiunta della bandiere

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,10 +1,22 @@
 <template>
   <div class="card">
       <ul>
-          <li>{{info.title}}</li>
-          <li>{{info.original_title}}</li>
-          <li>{{info.original_language}}</li>
-          <li>{{info.vote_average}}</li>
+        <li>
+            <h4>TITOLO</h4>
+            <p>{{info.title}}</p>
+        </li>
+        <li>
+            <h4>TITOLO ORIGINALE</h4>
+            <p>{{info.original_title}}</p>
+        </li>
+        <li>
+            <h4>LINGUA</h4>
+            <p><i :class="'flag flag-' + (info.original_language)"></i></p>
+        </li>
+        <li>
+            <h4>VOTO</h4>
+            <p>{{info.vote_average}}</p>
+        </li>
       </ul>
   </div>
 </template>
@@ -14,10 +26,13 @@ export default {
     name: 'Card',
     props: {
         info: Object,
-    }
+    },
 }
 </script>
 
 <style>
-
+    @import '~mdb-ui-kit/css/mdb.min.css';
+    .card{
+        border: 1px solid black;
+    }
 </style>
